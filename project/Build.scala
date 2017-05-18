5eec72680045dddf59e528df9c008f9911e920db
biomunky <biomunky@gmail.com>
2017-05-18 21:51:56 +0100

    remove scalaVersion. add types to vals

--- project/Build.scala
+++ project/Build.scala
@@ -1,18 +1,14 @@
-import sbt._
-
-
 object Resolvers {
   lazy val releases = "http://my.maven.repo/"
   lazy val Snapshots = "http://my.maven.repo/SNAPSHOTS"
 }
 
 object BuildConfig {
-  val scalaVersion = "2.11.7"
   val scalacOptions = Seq("-deprecation", "-unchecked", "-feature", "-encoding", "uft8")
-  val envBuildNumber = env("BUILD_NUMBER")
-  val buildNumber = if (envBuildNumber.nonEmpty) envBuildNumber else "0.0.1-SNAPSHOT"
-  val buildWhen = (new java.util.Date).toString
-  val buildMachine = java.net.InetAddress.getLocalHost.getHostName
+  val envBuildNumber: String = env("BUILD_NUMBER")
+  val buildNumber: String = if (envBuildNumber.nonEmpty) envBuildNumber else "0.0.1-SNAPSHOT"
+  val buildWhen: String = (new java.util.Date).toString
+  val buildMachine: String = java.net.InetAddress.getLocalHost.getHostName
   val artifactName = "bones"
 
   def env(key: String, default: String = ""): String = Option(System.getenv get key) getOrElse default
